015d2b6e2b0d195e4d7a299b1b65b1fb56d7f71c
Joshua Scott <joshsoragaming@gmail.com>
2017-09-10 05:14:23 +0100

    cpp: Add numeric types

--- LibProtocol.Targets/Cpp/src/protocol/KryptonType.h
+++ LibProtocol.Targets/Cpp/src/protocol/KryptonType.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <iostream>
 
+#if !defined(WRITE_LITTLE_ENDIAN) && !defined(WRITE_BIG_ENDIAN)
+#define WRITE_BIG_ENDIAN
+#endif
+
 namespace krypton {
 class KryptonType {
 public:
@@ -13,5 +17,5 @@ public:
      * Populates the type with data read from the BufferReader
      */    
     virtual void read(std::istream& is) = 0;
-}
+};
 }
---
+++ LibProtocol.Targets/Cpp/src/protocol/kryptonnumeric.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <algorithm>
+#include "KryptonType.h"
+
+/**
+ * Helper function for determining system endianness
+ */
+bool is_big_endian() {
+    union {
+        uint32_t i;
+        uint8_t c[4];
+    } bint = {0x01020304};
+    return bint.c[0] == 0x01;
+}
+
+namespace krypton {
+template <
+    typename ValueT,
+    typename = typename std::enable_if<std::is_arithmetic<ValueT>::value, ValueT>::type
+>
+class KryptonNumeric : KryptonType {
+public:
+    union DataValue {
+        char data[sizeof(ValueT)];
+        ValueT value;
+	};
+    
+    KryptonNumeric() { m_value = 0; }
+    KryptonNumeric(ValueT value) { m_value = value; }
+
+    void write(std::ostream& os) const {
+        DataValue dv;
+        dv.value = m_value;
+        
+#ifdef WRITE_BIG_ENDIAN
+        if (!is_big_endian())
+#else
+        if (is_big_endian())
+#endif
+            std::reverse(&dv.data[0], &dv.data[sizeof(ValueT)]);
+
+        os.write(dv.data, sizeof(ValueT));
+    }
+
+    void read(std::istream& is) {
+        DataValue dv;
+        is.read(&dv.data[0], sizeof(ValueT));
+
+#ifdef WRITE_BIG_ENDIAN
+		if (!is_big_endian())
+#else
+		if (is_big_endian())
+#endif
+			std::reverse(&dv.data[0], &dv.data[sizeof(ValueT)]);
+
+        m_value = dv.value;
+    }
+
+	ValueT value() const {
+		return m_value;
+	}
+private:
+    ValueT m_value;
+};
+
+typedef KryptonNumeric<int8_t> KryptonInt8;
+typedef KryptonInt8 kint8_t;
+typedef KryptonNumeric<uint8_t> KryptonUInt8;
+typedef KryptonUInt8 kuint8_t;
+
+typedef KryptonNumeric<int16_t> KryptonInt16;
+typedef KryptonInt16 kint16_t;
+typedef KryptonNumeric<uint16_t> KryptonUInt16;
+typedef KryptonUInt16 kuint16_t;
+
+typedef KryptonNumeric<int32_t> KryptonInt32;
+typedef KryptonInt32 kint32_t;
+typedef KryptonNumeric<uint32_t> KryptonUInt32;
+typedef KryptonUInt32 kuint32_t;
+
+typedef KryptonNumeric<int64_t> KryptonInt64;
+typedef KryptonInt64 kint64_t;
+typedef KryptonNumeric<uint64_t> KryptonUInt64;
+typedef KryptonUInt64 kuint64_t;
+}
